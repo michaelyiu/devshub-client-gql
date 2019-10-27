cff4ae73d4afdada184f995112b3c4c5e71154e0
unknown <onew1ng3d@gmail.com>
2019-10-27 18:45:18 -0400

    apollo-cache-persist added, seems to persist through browser reset

--- src/apollo.js
+++ src/apollo.js
@@ -1,10 +1,15 @@
 
 import ApolloClient, { InMemoryCache } from "apollo-boost";
+import { persistCache } from "apollo-cache-persist";
 
-
+const cache = new InMemoryCache();
+persistCache({
+	cache,
+	storage: localStorage
+})
 const client = new ApolloClient({
 	uri: "http://localhost:4000/graphql",
-	cache: new InMemoryCache(),
+	cache,
 	resolvers: {
 		Mutation: {
 			changeValue: (_, args, { cache }) => {
