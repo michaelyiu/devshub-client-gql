7f8e076110b3ee159bed634f7f18711480dd2d65
unknown <onew1ng3d@gmail.com>
2019-11-02 11:44:31 -0400

    localStorage set/get persistence implemented

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,10 +1,16 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {
-	const [isAuthenticated, setAuth] = useState(false);
-	const [currentUser, setCurrentUser] = useState({})
+	const [isAuthenticated, setAuth] = useState(() => {
+		const localData = localStorage.getItem('isAuthenticated');
+		return localData ? JSON.parse(localData) : false;
+	});
+	const [currentUser, setCurrentUser] = useState(() => {
+		const localData = localStorage.getItem('currentUser');
+		return localData ? JSON.parse(localData) : {};
+	})
 	const toggleAuth = () => {
 		setAuth(!isAuthenticated)
 	}
@@ -14,6 +20,12 @@ const AuthContextProvider = (props) => {
 	const clearCurrentUser = () => {
 		setCurrentUser({})
 	}
+	useEffect(() => {
+		localStorage.setItem('isAuthenticated', isAuthenticated)
+	}, [isAuthenticated])
+	useEffect(() => {
+		localStorage.setItem('currentUser', JSON.stringify(currentUser))
+	}, [currentUser])
 	return (
 		<AuthContext.Provider value={{ isAuthenticated, toggleAuth, currentUser, addCurrentUser, clearCurrentUser }}>
 			{props.children}
