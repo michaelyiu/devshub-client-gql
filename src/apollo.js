bb0f106af389885115c5c8fce83ca935575b7dab
unknown <onew1ng3d@gmail.com>
2019-11-16 01:50:52 -0500

    messed up uri link

--- src/apollo.js
+++ src/apollo.js
@@ -5,7 +5,7 @@ const client = new ApolloClient({
 	// uri: process.env.NODE_ENV === 'development'
 	// 	? 'http://localhost:9000/.netlify/functions/graphql'
 	// 	: '/.netlify/functions/graphql',
-	uri: 'https://mystifying-carson-b00552.netlify.com/.netlify/functions/graphql/graphql',
+	uri: 'https://mystifying-carson-b00552.netlify.com/.netlify/functions/graphql',
 	request: async operation => {
 		const token = window.localStorage.getItem('token');
 		operation.setContext({
