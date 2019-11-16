0455c22db558fc8be5cd0bf60c7bc3ae5d00a491
unknown <onew1ng3d@gmail.com>
2019-11-16 01:38:26 -0500

    uri to netlify

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,10 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000/graphql",
+	// uri: process.env.NODE_ENV === 'development'
+	// 	? 'http://localhost:9000/.netlify/functions/graphql'
+	// 	: '/.netlify/functions/graphql',
+	uri: 'https://mystifying-carson-b00552.netlify.com/.netlify/functions/graphql/graphql',
 	request: async operation => {
 		const token = window.localStorage.getItem('token');
 		operation.setContext({
